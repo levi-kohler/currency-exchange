f7a934c01c2e5277e9c61fbe66b3dd18a5e8f391
Levi Kohler <kohler.la01@gmail.com>
2021-02-12 11:45:56 -0800

    write getElements function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Converter from './js/converter.js'
+
+function getElements(response) {
+  if (response.conversion_rate) {
+    $('showTotal').text(`The total in GBP is ${response.conversion_rate}`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
+
