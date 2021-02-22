cae269037ae3ea3095b1332a3ce07eedb79ab2c8
Levi Kohler <kohler.la01@gmail.com>
2021-02-21 21:31:23 -0800

    add clearfields function

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Converter from './js/converter.js';
 
+function clearFields() {
+  $('.showErrors').text('');
+  $('.showTotal').text('');
+  $('#USD').val('');
+}
+
 function displayConversion(response, input, currency) {
   if (response.result != 'success') {
     $('.showErrors').text(`There was an error: ${response.error}`);
@@ -22,7 +28,8 @@ function displayConversion(response, input, currency) {
 $(document).ready(function() {
   $('#convert').click(function() {
     let input = $('#USD').val();
-    let currency = $('#currency option:selected').val();
+    let currency = $('#currency').val();
+    clearFields();
     Converter.getConversion(currency, input)
       .then(function(response) {
         displayConversion(response, input, currency);
