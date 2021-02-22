ba6fdc26af0d8268e9c35f6d9ced2a7c24418b2e
Levi Kohler <kohler.la01@gmail.com>
2021-02-21 17:41:24 -0800

    fix issue with API key not connecting properly, had to add dotenv to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -19,6 +20,7 @@ module.exports = {
       template: './src/index.html',
       inject: 'body'
     }),
+    new Dotenv()
   ],
   module: {
     rules: [
